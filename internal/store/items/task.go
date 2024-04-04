170a9491b747a6783b769866481e8f4e5994cc6f
Fred Yeh <b0093069@gmail.com>
2024-04-04 15:31:19 +0800

    enum implement for Task.Status

--- internal/store/items/task.go
+++ internal/store/items/task.go
@@ -1,14 +1,53 @@
 package items
 
-type Task struct {
-	Name   string `json:"name" redis:"name"`
-	Status int    `json:"status" redis:"status"`
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type Status int
+
+const (
+	Todo Status = iota
+	Done
+)
+
+func (s Status) String() string {
+	return map[int]string{0: "Todo", 1: "Done"}[int(s)]
 }
 
-func (t *Task) Unify() {
-	if t.Status > 1 {
-		t.Status = 1
-	} else if t.Status < 0 {
-		t.Status = 0
+func (s *Status) MarshalJSON() ([]byte, error) {
+	if *s != 0 && *s != 1 {
+		return nil, fmt.Errorf("%v is not a valid status", *s)
 	}
+	return json.Marshal(s.String())
+}
+
+func (s *Status) UnmarshalJSON(data []byte) error {
+	var status string
+	err := json.Unmarshal(data, &status)
+	if err != nil {
+		return err
+	}
+	*s, err = ParseStatus(status)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ParseStatus(s string) (Status, error) {
+	s = strings.TrimSpace(strings.ToLower(s))
+	value, ok := map[string]int{"todo": 0, "done": 1}[s]
+	if !ok {
+		return Status(0), fmt.Errorf("%v is not a valid status", s)
+	} else {
+		return Status(value), nil
+	}
+}
+
+type Task struct {
+	Name   string `json:"name" redis:"name"`
+	Status Status `json:"status" redis:"status"`
 }
--- internal/store/items/task_test.go
+++ internal/store/items/task_test.go
@@ -1,22 +1,44 @@
 package items
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnify(t *testing.T) {
-	task := Task{Name: "test", Status: 0}
-	task.Unify()
-	assert.Equal(t, Task{Name: "test", Status: 0}, task)
-	task = Task{Name: "test", Status: 1}
-	task.Unify()
-	assert.Equal(t, Task{Name: "test", Status: 1}, task)
+func TestTask(t *testing.T) {
+	task := Task{Name: "test", Status: Todo}
+	data, _ := json.Marshal(&task)
+	expected, _ := json.Marshal(map[string]any{"name": "test", "status": "Todo"})
+	assert.Equal(t, string(expected), string(data))
+
+	task = Task{Name: "test", Status: Done}
+	data, _ = json.Marshal(&task)
+	expected, _ = json.Marshal(map[string]any{"name": "test", "status": "Done"})
+	assert.Equal(t, string(expected), string(data))
+
 	task = Task{Name: "test", Status: 3}
-	task.Unify()
-	assert.Equal(t, Task{Name: "test", Status: 1}, task)
-	task = Task{Name: "test", Status: -5}
-	task.Unify()
-	assert.Equal(t, Task{Name: "test", Status: 0}, task)
+	data, err := json.Marshal(&task)
+	assert.Equal(t, "", string(data))
+	assert.Errorf(t, err, "3 is not a valid stutas")
+
+	task = Task{}
+	data = []byte(`{"name": "test", "status": "Todo"}`)
+	err = json.Unmarshal(data, &task)
+	assert.NoError(t, err)
+	assert.Equal(t, Task{Name: "test", Status: Todo}, task)
+	assert.Equal(t, 0, int(task.Status))
+
+	task = Task{}
+	data = []byte(`{"name": "test", "status": "Done"}`)
+	err = json.Unmarshal(data, &task)
+	assert.NoError(t, err)
+	assert.Equal(t, Task{Name: "test", Status: Done}, task)
+	assert.Equal(t, 1, int(task.Status))
+
+	task = Task{}
+	data = []byte(`{"name": "test", "status": "Hello"}`)
+	err = json.Unmarshal(data, &task)
+	assert.Errorf(t, err, "Hello is not a valid stataus")
 }

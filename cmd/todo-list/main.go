4673680028098a58146272352538643a3a20955f
Fred Yeh <b0093069@gmail.com>
2024-04-08 08:37:38 +0800

    feat: graceful-shutdown

--- cmd/todo-list/main.go
+++ cmd/todo-list/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/FredYeh/todo-list/internal/router"
 	"github.com/spf13/viper"
@@ -13,10 +18,36 @@ func main() {
 	if argvLen := len(os.Args); argvLen > 1 {
 		config = os.Args[1]
 	}
-	app := router.Router(config)
+	router := router.Router(config)
 
-	svr := viper.GetString("application.address") + ":" + viper.GetString("application.port")
-	if err := app.Run(svr); err != nil {
-		log.Fatal(err)
+	svr := &http.Server{
+		Addr:    viper.GetString("application.address") + ":" + viper.GetString("application.port"),
+		Handler: router,
 	}
+
+	go func() {
+		if err := svr.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := svr.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
